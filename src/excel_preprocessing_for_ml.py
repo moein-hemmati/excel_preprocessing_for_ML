779aed92e353a65fba926a0cc27ba29fa780846c
Moein Hemmati <moeinh@uark.edu>
2026-05-04 23:15:49 -0500

    Update excel_preprocessing_for_ml.py

--- src/excel_preprocessing_for_ml.py
+++ src/excel_preprocessing_for_ml.py
@@ -21,7 +21,7 @@ def encode_excel(input_path):
     print("Categorical columns found:")
     print(list(categorical_columns))
 
-    # Apply one-hot encoding
+    # Apply one-hot encoding zz
     df_encoded = pd.get_dummies(df, columns=categorical_columns)
 
     # Create output path (same folder, new name)
